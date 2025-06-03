e8fb64e834768d151ccf5f52b560c28643ca835b
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-06-03 19:33:36 +0700

    init command

--- index.ts
+++ index.ts
@@ -1,8 +1,43 @@
-import { randomUUIDv7 } from 'bun'
-
-console.log('Hello 望音!')
-if (randomUUIDv7()) {
-	console.log('chat is this true')
-} else {
-	console.log('chat is this is not true')
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits, Collection } from 'discord.js'
+import helloCommand from './commands/domains/tools/hello'
+declare module 'discord.js' {
+	export interface Client {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		commands: Collection<any, any>
+	}
 }
+const token = process.env.DISCORD_API_KEY
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+})
+
+client.commands = new Collection()
+client.commands.set('bruh', helloCommand)
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return
+
+	const command = client.commands.get(interaction.commandName)
+
+	if (!command) {
+		console.error(`No command found for ${interaction.commandName}`)
+		return
+	}
+
+	try {
+		await command.execute(interaction)
+	} catch (error) {
+		console.error(error)
+		await interaction.reply({
+			content: 'There was an error executing this command.',
+			ephemeral: true,
+		})
+	}
+})
+
+client.login(token)
