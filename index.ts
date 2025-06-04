52e482099d3b70326b40f4a8cb42f2a18cac5898
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-06-04 09:55:30 +0700

    add functtion get time

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // Require the necessary discord.js classes
 import { Client, Events, GatewayIntentBits, Collection } from 'discord.js'
 import helloCommand from './commands/domains/tools/hello'
+import getTimeCommand from './commands/domains/tools/get_time'
 declare module 'discord.js' {
 	export interface Client {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -17,7 +18,9 @@ client.once(Events.ClientReady, readyClient => {
 })
 
 client.commands = new Collection()
-client.commands.set('bruh', helloCommand)
+client.commands.set(helloCommand.data.name, helloCommand)
+client.commands.set(getTimeCommand.data.name, getTimeCommand)
+
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return
