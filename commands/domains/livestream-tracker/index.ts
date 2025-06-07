4d42d9473f7ef0079bccef5eb7583eb1857184c4
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2025-06-07 07:47:06 +0700

    Feature/livestream resolver (#6)
    * feat livestream resolver
    
    * livestream resolver

--- commands/domains/livestream-tracker/index.ts
+++ commands/domains/livestream-tracker/index.ts
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js'
+import type { Livestream } from '../../../data_type/livestream.ts'
 const link = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&eventType=upcoming&channelId=${process.env.TEST_CHANNEL_ID}&key=${process.env.YOUTUBE_API_KEY}`
 
 export default {
@@ -11,8 +12,13 @@ export default {
 			if (!response.ok) {
 				throw new Error(`Network response was not ok. Status: ${response.status}`)
 			}
-			const result = await response.json()
-			await interaction.reply(JSON.stringify(result))
+			const result = await response.json() as Livestream
+			if (result.items.length === 0) {
+				await interaction.reply('No upcoming streams found.')
+				return
+			}
+			const stream = result.items[0]
+			await interaction.reply(stream?.snippet.title ?? '')
 		} catch (error) {
 			console.error('Error fetching livestream info:', error)
 			await interaction.reply({
