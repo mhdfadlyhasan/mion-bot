9e4dd851ff4b4b819bea2d93b000176b65ba0448
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-06-07 07:46:27 +0700

    feat livestream resolver

--- commands/domains/livestream-tracker/index.ts
+++ commands/domains/livestream-tracker/index.ts
@@ -1,4 +1,5 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js'
+import { SlashCommandBuilder, ChatInputCommandInteraction, MessagePayload } from 'discord.js'
+import type { Livestream } from '../../../data_type/livestream'
 const link = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&eventType=upcoming&channelId=${process.env.TEST_CHANNEL_ID}&key=${process.env.YOUTUBE_API_KEY}`
 
 export default {
@@ -11,8 +12,8 @@ export default {
 			if (!response.ok) {
 				throw new Error(`Network response was not ok. Status: ${response.status}`)
 			}
-			const result = await response.json()
-			await interaction.reply(JSON.stringify(result))
+			const result = await response.json() as Livestream
+			await interaction.reply(result.items[0].snippet.title)
 		} catch (error) {
 			console.error('Error fetching livestream info:', error)
 			await interaction.reply({
