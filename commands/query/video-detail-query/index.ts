c83913c0d7a5489bd2165851f861375f17f0d0fa
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-06-13 09:23:32 +0700

    enh: send scheduled time

---
+++ commands/query/video-detail-query/index.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js'
+import type { Youtuber } from '../../../data_type/youtuber'
+import type { Livestream } from '../../../data_type/livestream.ts'
+import { redisSet, redisGet } from '../../../tools/redis.ts/index.ts'
+
+export async function getVideoDetail(idList: string[]): Promise<Livestream | string | null> {
+	try {
+		const link = `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${idList.join(',')}&key=${process.env.YOUTUBE_API_KEY}`
+		const response = await fetch(link)
+		if (!response.ok) {
+			throw new Error(`Network response was not ok. Status: ${response.status}`)
+		}
+		const livestream = await response.json() as Livestream
+		if (livestream.items.length === 0) {
+			return null
+		}
+		console.log(livestream.items[0]?.liveStreamingDetails)
+		return livestream
+	} catch (error) {
+		console.error('Error fetching livestream info:', error)
+		return error as string
+	}
+}
