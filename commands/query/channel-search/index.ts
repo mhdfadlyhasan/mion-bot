3014f03330bd932f6f3e29e7216b5aa643e16ff7
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-06-15 10:30:20 +0700

    Enh: use wildcard in redis key

--- commands/query/channel-search/index.ts
+++ commands/query/channel-search/index.ts
@@ -1,9 +1,8 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js'
 import type { Youtuber } from '../../../data_type/youtuber'
 import type { Livestream } from '../../../data_type/livestream.ts'
-import { redisSet, redisGet, redisSetJson } from '../../../tools/redis.ts/index.ts'
+import { redisSetJson, redisGetWildCard } from '../../../tools/redis.ts/index.ts'
 import { getVideoDetail } from '../video-detail-query/index.ts'
-
 
 export default {
 	data: new SlashCommandBuilder()
@@ -17,7 +16,7 @@ export default {
 	async execute(interaction: ChatInputCommandInteraction) {
 		try {
 			const name = interaction.options.getString('name', true)
-			const linkInCache = await redisGet(name)
+			const linkInCache = await redisGetWildCard(name.toLowerCase())
 			if (linkInCache !== null) {
 				console.info('using data from redis')
 				const detail = JSON.parse(linkInCache) as Youtuber
@@ -34,7 +33,6 @@ export default {
 				await interaction.reply('No channels found.')
 				return
 			}
-
 			const linkStream = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&eventType=upcoming&channelId=${youtuber.items[0]?.id.channelId}&key=${process.env.YOUTUBE_API_KEY}`
 			const latestStream = await fetch(linkStream)
 			if (!latestStream.ok) {
@@ -57,14 +55,13 @@ export default {
 				startTime = new Date(videoInfo.items[0]?.liveStreamingDetails.scheduledStartTime).toLocaleString("en-US", { timeZone: "Asia/Bangkok" })
 			}
 			await interaction.reply('Live time ' + startTime + '\n' + youtuber.items[0]?.snippet.channelTitle + 'https://www.youtube.com/watch?v=' + stream!.id.videoId)
-			redisSet(youtuber.items[0]?.snippet.channelTitle as string, stream!.id.videoId)
 			youtuber.latestStreamLink = 'https://www.youtube.com/watch?v=' + stream!.id.videoId
 			youtuber.latestStreamTime = startTime
-			redisSetJson(youtuber.items[0]?.snippet.channelTitle as string, youtuber)
+			redisSetJson(youtuber.items[0]?.snippet.channelTitle.toLowerCase() as string, youtuber)
 		} catch (error) {
 			console.error('Error fetching youtuber info:', error)
 			await interaction.reply({
-				content: 'Failed to fetch youtuber info.',
+				content: 'Failed to fetch youtuber info.' + error,
 				ephemeral: true,
 			})
 		}
