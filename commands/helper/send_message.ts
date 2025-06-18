472b3261da386974e05461729adcd6ae2996e796
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-06-18 13:59:23 +0700

    Feat: remind stream at time

--- commands/helper/send_message.ts
+++
@@ -1,7 +0,0 @@
-import { ChannelType, type Channel } from 'discord.js'
-
-export function sendMessage(channel: Channel, message: string) {
-	if (channel?.type === ChannelType.GuildText) {
-		channel.send(message)
-	}
-}
---
+++ tools/client/index.ts
@@ -0,0 +1,66 @@
+import { ChannelType, Client, Collection, Events, GatewayIntentBits, type Channel } from 'discord.js'
+import helloCommand from '../../commands/helper/hello'
+import getTimeCommand from '../../commands/helper/get_time'
+import livestreamCommand from '../../commands/domains/livestream-tracker'
+import channelSearch from '../../commands/query/channel-search'
+const chatClient = await new Client({ intents: [GatewayIntentBits.Guilds] })
+var channel: Channel | undefined
+const token = process.env.DISCORD_API_KEY!
+
+export function sendMessage(message: string) {
+	console.log(channel)
+	if (channel?.type === ChannelType.GuildText) {
+		channel.send(message)
+	}
+}
+
+declare module 'discord.js' {
+	export interface Client {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		commands: Collection<any, any>
+	}
+}
+chatClient.once(Events.ClientReady, async readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+	channel = chatClient.channels.cache.get(process.env.TEST_DISCORD_CHANNEL_ID!)
+	sendMessage('こんにちは, bot is starting!')
+})
+
+chatClient.once(Events.ShardDisconnect, disconnectClient => {
+	sendMessage(`Disconnecting! ${disconnectClient.code}`)
+})
+
+chatClient.commands = new Collection()
+chatClient.commands.set(helloCommand.data.name, helloCommand)
+chatClient.commands.set(getTimeCommand.data.name, getTimeCommand)
+chatClient.commands.set(livestreamCommand.data.name, livestreamCommand)
+chatClient.commands.set(channelSearch.data.name, channelSearch)
+
+
+chatClient.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return
+
+	const command = chatClient.commands.get(interaction.commandName)
+
+	if (!command) {
+		console.error(`No command found for ${interaction.commandName}`)
+		return
+	}
+
+	try {
+		await command.execute(interaction)
+	} catch (error) {
+		console.error(error)
+		await interaction.reply({
+			content: 'There was an error executing this command.',
+			ephemeral: true,
+		})
+	}
+})
+
+chatClient.login(token)
+
+export default {
+	ChatClient: chatClient,
+	Channel: channel,
+}
