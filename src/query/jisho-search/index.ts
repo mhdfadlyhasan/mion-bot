570e58c13e1a98a38fc2bd421cab0a87927ad6f6
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2025-06-28 10:08:47 +0700

    Enh: support multi entry in a word (#25)

--- src/query/jisho-search/index.ts
+++ src/query/jisho-search/index.ts
@@ -1,5 +1,5 @@
 import type { JishoEntry } from '../../data_type/jisho_entry'
-
+import { tokenize } from "kuromojin"
 export default async function jishoSearch(word: string): Promise<string> {
 	type JishoResponse = {
 		meta: {
@@ -7,13 +7,27 @@ export default async function jishoSearch(word: string): Promise<string> {
 		}
 		data: JishoEntry[]
 	}
+	async function splitJapanese(text: string) {
+		const tokens = await tokenize(text)
+		return tokens.map(t => ({
+			surface: t.surface_form,
+			reading: t.reading,
+			basicForm: t.basic_form
+		}))
+	}
+	const wordList = await splitJapanese(word)
+	console.log(wordList)
 
 	try {
-		const link = `https://jisho.org/api/v1/search/words?keyword=${word}`
-		const response = await fetch(link)
-		const jsonResponse = await response.json() as JishoResponse
-		const jishoEntry = jsonResponse.data[0] as JishoEntry
-		const result = `**${jishoEntry.slug}** (${jishoEntry.is_common ? 'common' : 'uncommon'})\n Reading:[**${jishoEntry.japanese[0].reading}**], Meaning: ${jishoEntry.senses[0].english_definitions.join(', ')}`
+		let result: string = ""
+		for (const word of wordList) {
+			const link = `https://jisho.org/api/v1/search/words?keyword=${word.surface}`
+			const response = await fetch(link)
+			const jsonResponse = await response.json() as JishoResponse
+			const jishoEntry = jsonResponse.data[0] as JishoEntry
+			const entry = `**${jishoEntry.slug}** (${jishoEntry.is_common ? 'common' : 'uncommon'})\n Reading:[**${jishoEntry.japanese[0].reading}**], Meaning: ${jishoEntry.senses[0].english_definitions.join(', ')}\n`
+			result += entry
+		}
 		return result
 	} catch (error) {
 		console.error('Error fetching jisho info:', error)
