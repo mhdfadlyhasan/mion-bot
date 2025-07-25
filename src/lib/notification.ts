2d59c87c075d31a7ff7bb405e2b39257dff9b3ae
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-07-26 04:51:10 +0700

    reduce cpu usage by reduce notif

--- src/lib/notification.ts
+++ src/lib/notification.ts
@@ -1,9 +1,12 @@
 import { sendMessage } from '../tools/client'
 
 const notificationMap: { [key: string]: NodeJS.Timeout | null } = {}
+const now = Date.now()
+const ONE_HOUR = 1000 * 60 * 60
 
 export async function setNotification(name: string, message: string, delay: number) {
-	if (delay > 0) {
+	const isStartingSoon = delay > 0 && delay <= ONE_HOUR
+	if (isStartingSoon) {
 		if (notificationMap[name] != null) {
 			clearTimeout(notificationMap[name])
 			console.log('clearing notification')
