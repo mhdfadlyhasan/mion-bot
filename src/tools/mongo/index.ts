337526e270e48d04c434be6246ed066548807410
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-07-29 11:14:19 +0700

    fix implement mongodb

--- src/tools/mongo/index.ts
+++ src/tools/mongo/index.ts
@@ -1,9 +1,9 @@
 import { ObjectId, MongoClient } from "mongodb"
 
-const mongoClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING!)
-	.on('error', (err) => console.log('mongo Client Error', err))
-	.connect()
+// const mongoClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING!)
+// 	.on('error', (err) => console.log('mongo Client Error', err))
+// 	.connect()
 
-export async function mongoSet(key: string, input: string): Promise<string | null> {
-	return await mongoClient.set(key, input)
-}
+// export async function mongoSet(key: string, input: string): Promise<string | null> {
+// 	return await mongoClient.set(key, input)
+// }
