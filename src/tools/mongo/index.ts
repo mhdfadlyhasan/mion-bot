5bfed63a7f36c37ec22b665892b5ad30e1a7b0c7
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-07-31 07:08:21 +0700

    disable connection to mongo

--- src/tools/mongo/index.ts
+++ src/tools/mongo/index.ts
@@ -1,12 +1,12 @@
 import { ObjectId, MongoClient, Db, Collection } from "mongodb"
 
 export async function connectToDatabase() {
-	const client: MongoClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING!)
-	await client.connect()
-	const dbName = "server"
-	const db: Db = client.db(dbName)
+	// const client: MongoClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING!)
+	// await client.connect()
+	// const dbName = "server"
+	// const db: Db = client.db(dbName)
 
-	const serverCollection: Collection = db.collection(dbName)
+	// const serverCollection: Collection = db.collection(dbName)
 
-	console.log(`Successfully connected to database: ${db.databaseName} and collection: ${serverCollection.collectionName}`)
+	// console.log(`Successfully connected to database: ${db.databaseName} and collection: ${serverCollection.collectionName}`)
 }
